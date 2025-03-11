9866557cca527700cc845281fbc7d24e71e56d17
GhaniRizqi <ghannimuhhh@gmail.com>
2025-03-11 10:25:02 +0700

    Update project

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Maafffffffffffffffffffffffffffffffffff kemaren nolakkkkkkkk').split('')
+    const titles = ('Maaff kemaren nolakkk').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
